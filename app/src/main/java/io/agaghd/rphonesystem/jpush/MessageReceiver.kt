64ddfc26246dd94010b402ec37c2730114118683
agaghd <cover_story@163.com>
2019-09-28 15:00:05 +0800

    修改广域网推送指令。

--- app/src/main/java/io/agaghd/rphonesystem/jpush/MessageReceiver.kt
+++ app/src/main/java/io/agaghd/rphonesystem/jpush/MessageReceiver.kt
@@ -1,13 +1,75 @@
 package io.agaghd.rphonesystem.jpush
 
+import android.app.Notification
 import android.content.Context
+import android.content.Intent
 import android.util.Log
+import android.widget.Toast
+import cn.jpush.android.api.CmdMessage
+import cn.jpush.android.api.CustomMessage
 import cn.jpush.android.api.JPushMessage
+import cn.jpush.android.api.NotificationMessage
 import cn.jpush.android.service.JPushMessageReceiver
+import io.agaghd.rphonesystem.flashlight.FlashLigntUtil
 
 class MessageReceiver : JPushMessageReceiver() {
     override fun onTagOperatorResult(p0: Context?, p1: JPushMessage?) {
         super.onTagOperatorResult(p0, p1)
-        Log.i("jiguang", p1.toString())
+    }
+
+    override fun onCheckTagOperatorResult(p0: Context?, p1: JPushMessage?) {
+        super.onCheckTagOperatorResult(p0, p1)
+    }
+
+    override fun onNotifyMessageDismiss(p0: Context?, p1: NotificationMessage?) {
+        super.onNotifyMessageDismiss(p0, p1)
+    }
+
+    /**
+     * 处理返回内容
+     */
+    override fun getNotification(p0: Context?, p1: NotificationMessage?): Notification {
+        val content = if (p1 != null) p1.notificationContent else ""
+        when (content) {
+            FlashLigntUtil.TOGGLE_ORDER -> FlashLigntUtil.toggleTouchLight()
+            else -> Log.i("wtf", content)
+        }
+        return super.getNotification(p0, p1)
+    }
+
+    override fun onNotifyMessageOpened(p0: Context?, p1: NotificationMessage?) {
+        super.onNotifyMessageOpened(p0, p1)
+    }
+
+    override fun onNotifyMessageArrived(p0: Context?, p1: NotificationMessage?) {
+        super.onNotifyMessageArrived(p0, p1)
+    }
+
+    override fun onRegister(p0: Context?, p1: String?) {
+        super.onRegister(p0, p1)
+    }
+
+    override fun onCommandResult(p0: Context?, p1: CmdMessage?) {
+        super.onCommandResult(p0, p1)
+    }
+
+    override fun onMessage(p0: Context?, p1: CustomMessage?) {
+        super.onMessage(p0, p1)
+    }
+
+    override fun onConnected(p0: Context?, p1: Boolean) {
+        super.onConnected(p0, p1)
+    }
+
+    override fun onMobileNumberOperatorResult(p0: Context?, p1: JPushMessage?) {
+        super.onMobileNumberOperatorResult(p0, p1)
+    }
+
+    override fun onAliasOperatorResult(p0: Context?, p1: JPushMessage?) {
+        super.onAliasOperatorResult(p0, p1)
+    }
+
+    override fun onMultiActionClicked(p0: Context?, p1: Intent?) {
+        super.onMultiActionClicked(p0, p1)
     }
 }
